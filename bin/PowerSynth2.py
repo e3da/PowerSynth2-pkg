29fb3aed71fbe675dd7b5a3ea1489801a408e3ff
Yarui Peng <yrpeng@uark.edu>
2023-04-20 16:15:11 -0500

    decoupled import in PowerSynth2 main file

--- bin/PowerSynth2.py
+++ bin/PowerSynth2.py
@@ -3,14 +3,13 @@
 
 import sys
 
-from core.PS2Core import PS2Core
-from gui.PS2GUI import PS2GUI
-
 if __name__ == "__main__":  
     if len(sys.argv)<2:
+        from gui.PS2GUI import PS2GUI
         print("----------------------PowerSynth 2 v2.0: GUI version------------------")
         PS2GUI().run()
     else:
+        from core.PS2Core import PS2Core
         print("----------------------PowerSynth 2 v2.0: CLI version------------------")
         PS2Core(sys.argv[1],sys.argv[2] if len(sys.argv)>2 else "").run()
 
