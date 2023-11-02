90cc230f775043c7964b2def6af87a08cf661661
Yarui Peng <yrpeng@uark.edu>
2023-11-01 22:51:03 -0500

    use PSVers as version

--- bin/PowerSynth2.py
+++ bin/PowerSynth2.py
@@ -2,14 +2,16 @@
 # This is the main exe for PowerSynth2
 
 import sys
+from core.PSCore import PSEnv
 
 if __name__ == "__main__":  
+    print(f"----------------------PowerSynth 2 v{PSEnv.PSVers}:", end="")
     if len(sys.argv)<2:
         from gui.PS2GUI import PS2GUI
-        print("----------------------PowerSynth 2 v2.0: GUI version------------------")
+        print(" GUI version------------------")
         PS2GUI().run()
     else:
         from core.PS2CLI import PS2CLI
-        print("----------------------PowerSynth 2 v2.0: CLI version------------------")
+        print(" CLI version------------------")
         PS2CLI(sys.argv[1],sys.argv[2] if len(sys.argv)>2 else "").run()
 
