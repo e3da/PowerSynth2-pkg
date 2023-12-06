e9a41bc1ed14191d51a3da19197c24179acc3a30
Yarui Peng <yrpeng@uark.edu>
2023-12-05 20:45:37 -0600

    update main exe and lnk

--- misc/win/PowerSynth2.ps1
+++ misc/win/PowerSynth2.ps1
@@ -1,26 +1,21 @@
-#To create exe through ps2exe:
-#ps2exe H:\git\PowerSynth\pkg\misc\win\PowerSynth2.ps1 H:\git\PowerSynth\pkg\misc\win\PowerSynth2.exe -iconFile H:\git\PowerSynth\pkg\PowerSynth2.ico -title "PowerSynth2" -description "https://e3da.csce.uark.edu/release/PowerSynth"
-
-Write-Host -NoNewLine "Usage: "
+#To create exe through ps2exe:  for GUI only: -noConsole -noOutput 
+#ps2exe pkg\misc\win\PowerSynth2.ps1 pkg\misc\win\PowerSynth2.exe -iconFile pkg\PowerSynth2.ico -title "PowerSynth2" -description "https://e3da.csce.uark.edu/release/PowerSynth"
+param([switch] $help )	
 
 if ($PSScriptRoot) {
-	#running as script
-	cd $PSScriptRoot\..\..\..\
-	Write-Host -NoNewLine "python.exe pkg\bin\PowerSynth2.py" 
-} else {
-	#running as exe
-	Write-Host -NoNewLine "PowerSynth2.exe"
+	
+	if ($help)
+	{
+		Write-Host -NoNewLine "Usage [Run from PSRoot]: .\python.exe pkg\bin\PowerSynth2.py"
+		Write-Host " [macrofile: if not given, starts GUI, otherwise starts CLI. If the macrofile does not exist, creates it interactively.]"
+		Exit
+	}
+	
+	#change to PSRoot
+	cd $PSScriptRoot\..\..\..
 }
 
-Write-Host " [macrofile: if not given, starts GUI, otherwise starts CLI. If the macrofile does not exist, creates it interactively.]"
-
-$CurrentDirectory = (get-location).path
-Write-Host "INFO: Starting PowerSynth2 from $CurrentDirectory"
+$PSRoot = (get-location).path
+Write-Host "INFO: Starting PowerSynth2 from $PSRoot"
 
 .\python.exe ".\pkg\bin\PowerSynth2.py" $args
-
-if (! $?) { 
-	Write-Host "Press anykey to continue" -ForegroundColor Yellow
-    $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-}
-
