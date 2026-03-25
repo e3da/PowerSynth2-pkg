31b5101ea366852f1eb6e914dd9131f165d3b4e1
coelho <rodrigo.coelho@iisb.fraunhofer.de>
2026-03-25 17:25:11 +0100

    make matlab optional in post_install.sh

--- misc/constructor/post_install.sh
+++ misc/constructor/post_install.sh
@@ -2,7 +2,7 @@
 #post conda installation script for pip install and clone git codes
 
 #search for matlab >2022b
-MatlabSearch=`ls /usr/local/MATLAB/R202[3-9]*/VersionInfo.xml -1 2>/dev/null | tail -1` 
+MatlabSearch=`ls /usr/local/MATLAB/R202[3-9]*/VersionInfo.xml -1 2>/dev/null | tail -1`
 
 #default variables
 : "${PYVers:=3.10}" "${MatlabRoot:=/usr/local/MATLAB/R2022b}"
@@ -19,34 +19,41 @@ if [ ! -f "$PREFIX/bin/pip" ]; then
 	exit 10
 fi
 
+# MATLAB is now optional
+InstallMatlab=true
 if [ ! -f "$MatlabRoot/VersionInfo.xml" ]; then
 	if [ -f "$MatlabSearch" ]; then
 		MatlabRoot=`dirname "$MatlabSearch"`
 	else
-		echo "ERROR: Matlab is not found at $MatlabRoot. Post installation failed."
-		sleep 2
-		exit 99
+		echo "WARNING: Matlab is not found at $MatlabRoot. Skipping matlabengine installation."
+		InstallMatlab=false
 	fi
 fi
 
-echo "INFO: Install to PREFIX=$PREFIX with python $PYVers and MatlabRoot=$MatlabRoot"
+if [ "$InstallMatlab" = true ]; then
+	echo "INFO: Install to PREFIX=$PREFIX with python $PYVers and MatlabRoot=$MatlabRoot"
 
-mlvers=`sed -n 's/^.*<version>\([0-9]\+\.[0-9]\+\).*$/\1/p' $MatlabRoot/VersionInfo.xml`
-export LD_LIBRARY_PATH="$MatlabRoot/bin/glnxa64/:$LD_LIBRARY_PATH"
+	mlvers=`sed -n 's/^.*<version>\([0-9]\+\.[0-9]\+\).*$/\1/p' $MatlabRoot/VersionInfo.xml`
+	export LD_LIBRARY_PATH="$MatlabRoot/bin/glnxa64/:$LD_LIBRARY_PATH"
+
+	#pip install matlabengine
+	echo "INFO: Install MATLAB Engine"
+	for pkg in "matlabengine==$mlvers"
+	do
+		"$PREFIX/bin/pip" install "$pkg.*" $pipoption -t "$PREFIX/lib/python$PYVers/site-packages"
+	done
+else
+	echo "INFO: Install to PREFIX=$PREFIX with python $PYVers (no MATLAB)"
+fi
 
 #pip install additional packages not in anaconda
 echo "INFO: Install Additional Packages"
-for pkg in "matlabengine==$mlvers"
-do
-	"$PREFIX/bin/pip" install "$pkg.*" $pipoption -t "$PREFIX/lib/python$PYVers/site-packages"
-done
-
 for pkg in "jmetalpy~=1.6.0"
 do
 	"$PREFIX/bin/pip" install "$pkg" $pipoption --no-deps -t "$PREFIX/lib/python$PYVers/site-packages"
 done
 
-if [ -d "$PREFIX/pkg" ]; then 
+if [ -d "$PREFIX/pkg" ]; then
 	rm -rf "$PREFIX/lib/python$PYVers/site-packages/core" "$PREFIX/pkg" "$PREFIX/lib/python$PYVers/site-packages/gui"
 fi
 
